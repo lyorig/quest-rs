4fdfd5cce55ea21a5ff4590585fe7f07cb7471e7
Petr Šácha <petrik.sacha@gmail.com>
2025-10-13 20:29:56 +0200

    tests: basic field usage

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,33 @@ fn main() {
 
     game.main_loop();
 }
+
+#[cfg(test)]
+mod tests {
+    use sdl3_sys::keycode::SDLK_BACKSPACE;
+
+    use crate::field::{Field, FieldAction};
+
+    #[test]
+    fn field() {
+        let mut f = Field::new();
+
+        // The embedded Unicode scalar values represent non-breaking spaces.
+        assert!(f.process_str("Hello"));
+        assert!(!f.process_str(" \u{A0} \u{A0} "));
+
+        f.cursor = 5;
+        assert!(f.process_str(" World!"));
+        assert_eq!(f.text, "Hello World! \u{A0} \u{A0} ");
+
+        // Remove one non-breaking space.
+        f.cursor = 14;
+        assert!(matches!(
+            f.process_key(SDLK_BACKSPACE),
+            FieldAction::TextRemoved
+        ));
+
+        assert_eq!(f.cursor, 13);
+        assert_eq!(f.text, "Hello World!  \u{A0} ");
+    }
+}
