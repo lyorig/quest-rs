57c96dee489fac4d6a30339df23f733fe006161c
Petr Šácha <petrik.sacha@gmail.com>
2025-11-30 01:17:24 +0100

    blah

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,25 @@
-use std::{ffi::CStr, ptr::NonNull};
+use std::{
+    ffi::{CStr, c_char},
+    ptr::NonNull,
+};
 
 use halcyon::{
     defs::SdlResult,
     ttf::{Font, Text, TtfContext},
 };
 
+/// Returns a `NonNull<c_char>`, for use with `SdlResult`s.
+pub fn error_str(msg: &'static CStr) -> NonNull<c_char> {
+    unsafe { NonNull::new_unchecked(msg.as_ptr().cast_mut()) }
+}
+
 pub fn find_sized_font<'a>(
     ttf: &'a TtfContext,
     rel_path: &CStr,
     desired_height: f32,
 ) -> SdlResult<Font<'a>> {
-    const INCR: f32 = 1.;
-    let mut curr = 4.;
+    const INCR: f32 = 1.0;
+    let mut curr = 4.0;
 
     while curr < 256. {
         let f = Font::new(ttf, rel_path, curr)?;
@@ -23,5 +31,5 @@ pub fn find_sized_font<'a>(
         }
     }
 
-    Err(unsafe { NonNull::new_unchecked(c"Couldn't find suitable font".as_ptr().cast_mut()) })
+    Err(error_str(c"Couldn't find suitable font"))
 }
