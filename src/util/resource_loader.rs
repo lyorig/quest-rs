c601cabbda27ed1ac59dd7ea99ce42a7a85c7063
Petr Šácha <petrsacha@proton.me>
2026-08-12 15:50:56 +0200

    use `Box<str>` in ResourceLoader

--- src/util/resource_loader.rs
+++ src/util/resource_loader.rs
@@ -1,29 +1,29 @@
 use std::ffi::{CStr, CString};
 
-use halcyon::{Result, string::String};
+use halcyon::{Result, boxed::Box};
 
 /// A simple string wrapper that provides utility methods for
 /// constructing a [`std::path::Path`] relative to the directory
 /// of the program that's running.
 pub struct ResourceLoader {
-    root: String,
+    root: Box<str>,
 }
 
 impl ResourceLoader {
     pub fn from_pref() -> Result<Self> {
-        let root = halcyon::pref_path(c"cz.lyorig", c"quest")?;
-        Ok(Self { root })
+        halcyon::pref_path(c"cz.lyorig", c"quest").map(|s| Self {
+            root: s.into_boxed_str(),
+        })
     }
 
     pub fn resolve(&self, path: &str) -> Box<CStr> {
         const NUL_ERROR: &str =
             "ResourceLoader::resolve() should never be given a Path with embedded NUL bytes";
 
-        let root = self.root.to_str();
-        let total_len = root.len() + path.len() + 1;
+        let total_len = self.root.len() + path.len() + 1;
 
         let mut vec = Vec::<u8>::with_capacity(total_len);
-        vec.extend_from_slice(root.as_bytes());
+        vec.extend_from_slice(self.root.as_bytes());
         vec.extend_from_slice(path.as_bytes());
         vec.push(b'\0');
 
